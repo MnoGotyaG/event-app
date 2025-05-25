46205f06cde1b1145589f846866c53b6cea851ff
MnoGotyaG <mak.bugaev2014@yandex.ru>
2025-05-25 16:24:06 +0300

    sad

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,7 @@ class UserController {
       phone, 
       telegram, 
       password_hash
-    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) 
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
     RETURNING id, first_name, last_name, email`,
     [
       userData.first_name,
